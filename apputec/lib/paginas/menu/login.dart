0a22840a001c146a23310ba87737ed8c8fb283dd
Manuel0298 <123275898+Manuel0298@users.noreply.github.com>
2023-05-27 12:24:42 -0600

    loginccompletado

--- apputec/lib/paginas/menu/login.dart
+++ apputec/lib/paginas/menu/login.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:apputec/paginas/figura/tarjeta.dart';
 
 class Login extends StatelessWidget {
@@ -182,4 +182,146 @@ Container _passwordinput() {
         padding: EdgeInsets.symmetric(horizontal: 0, vertical: 5),
         child: Image.network("https://png.pngtree.com/png-vector/20191110/ourlarge/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg",fit: BoxFit.contain,));
   }
- 
+ */
+
+import 'package:flutter/material.dart';
+import 'package:apputec/widgets/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:apputec/services/firebase.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController IdController = TextEditingController();
+  TextEditingController CorreouserController = TextEditingController(text: "");
+  TextEditingController PassworduserController = TextEditingController(text: "");
+  TextEditingController tpUsuarioController = TextEditingController(text: "");
+
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    final List _post = [
+      'Pantalla #1',
+      'Pantalla #2',
+      'Pantalla #3',
+      'Pantalla #3',
+      'Pantalla #3'
+    ];
+    String usuarios;
+    String password;
+    return Scaffold(
+      /* appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+      ),*/
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    "Portal Educativo",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Iniciar session para comenzar",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Image.network(
+                      "https://static.wikia.nocookie.net/logopedia/images/8/85/UTEC_El_Salvador_Seal.png",
+                        width: 370,
+                        height: 200,),
+                    
+                      
+                  TextFormField(
+                    controller: IdController,
+                     keyboardType: TextInputType.number,
+                       decoration: textInputDecoration.copyWith(
+                        labelText: "Correo",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    onChanged: (val) {
+                      setState(() {
+                        CorreouserController.text = val;
+                      });
+                    },
+                    validator: (value) {
+                      return RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value!)
+                          ? null
+                          : "Favor Ingresar correo valido";
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  
+                  TextFormField(
+                    obscureText: true,
+                    decoration: textInputDecoration.copyWith(
+                        labelText: "Contraseña",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    validator: (val) {
+                      if (val!.length < 0) {
+                        return "El password debe ser mayor de 0 caracteres";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (val) {
+                      setState(() {
+                        PassworduserController.text = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Theme.of(context).primaryColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                        child: const Text(
+                          "Ingresar",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {}
+                        },
+                      )),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  login() {}
+}
